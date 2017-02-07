f8239eb92e8c7be69a6654bad53a1ef137d38f76
John Dias <joaodias@google.com>
2017-02-06 16:06:08 -0800

    taimen: setup initial build targeting mtp8998
    Test: built and flashed by hand
    Change-Id: I02a0b803c2e8d769fe07d7db78d260527f486614
    Signed-off-by: John Dias <joaodias@google.com>

---
+++ aosp_taimen.mk
@@ -0,0 +1,33 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+PRODUCT_NAME := aosp_taimen
+PRODUCT_DEVICE := taimen
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := AOSP on taimen
+PRODUCT_MANUFACTURER := Google
+PRODUCT_RESTRICT_VENDOR_FILES := all
+
+$(call inherit-product, device/qcom/msmcobalt/utils.mk)
+$(call inherit-product, device/google/taimen/device.mk)
+$(call inherit-product-if-exists, vendor/qcom/msmcobalt/device-vendor.mk)
+
+PRODUCT_PACKAGES += \
+    Launcher3
