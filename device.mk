e0507318a9cdae7d8a1fe622b1ffc5eb6c168f5d
John Dias <joaodias@google.com>
2017-02-24 13:58:40 -0800

    taimen: use taimen-kernel, not msmcobalt-kernel
    Test: built, booted
    Change-Id: Id3ea41d27deed09fff7aadf06f9611f1568d1bbc
    Signed-off-by: John Dias <joaodias@google.com>

--- device.mk
+++ device.mk
@@ -19,6 +19,7 @@ PRODUCT_AAPT_PREF_CONFIG := 560dpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 PRODUCT_HARDWARE := taimen
+TARGET_PREBUILT_KERNEL := device/google/taimen-kernel/Image.gz-dtb
 
 include device/qcom/msmcobalt/device-common.mk
 
