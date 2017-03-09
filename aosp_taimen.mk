8dd58cc30da847328291ca09594ef3446dd23d74
Thierry Strudel <tstrudel@google.com>
2017-03-09 15:08:45 -0800

    Move common inherit-product to wahoo
    Change-Id: If3a643419c542cdbb5ced136747c1ec433b193e1
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- aosp_taimen.mk
+++ aosp_taimen.mk
@@ -14,23 +14,15 @@
 # limitations under the License.
 #
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-# Enforce privapp-permissions whitelist
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.control_privapp_permissions=enforce
-
-PRODUCT_NAME := aosp_taimen
-PRODUCT_DEVICE := taimen
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on taimen
-PRODUCT_MANUFACTURER := Google
-PRODUCT_RESTRICT_VENDOR_FILES := all
-
 $(call inherit-product, device/google/taimen/device.mk)
 $(call inherit-product-if-exists, vendor/google_devices/taimen/device-vendor.mk)
 
 PRODUCT_PACKAGES += \
     Launcher3
+
+PRODUCT_RESTRICT_VENDOR_FILES := all
+PRODUCT_MANUFACTURER := Google
+PRODUCT_BRAND := Android
+PRODUCT_NAME := aosp_taimen
+PRODUCT_DEVICE := taimen
+PRODUCT_MODEL := AOSP on taimen
