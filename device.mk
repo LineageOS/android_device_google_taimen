0402af370dad5ecb091b2313beabd0abe4d64676
Vineeta Srivastava <vsrivastava@google.com>
2017-04-03 17:54:46 -0700

    Split fstab from wahoo
    Change-Id: Idb3b7b539ed7c2b6b7db91ca0b77bf1b35cbbd13

--- device.mk
+++ device.mk
@@ -41,4 +41,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/google/taimen/nfc/libnfc-nxp.taimen.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
+# TODO: move this to wahoo.
+PRODUCT_COPY_FILES += \
+    device/google/taimen/fstab.hardware:root/fstab.$(PRODUCT_HARDWARE)
 
