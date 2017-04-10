152692b641f2364a7d120d7ce35077efed8ab622
Ramanan Rajeswaran <ramanan@google.com>
2017-04-10 12:23:28 -0700

    Add aosp assets.
    Test: build muskie.
    
    Change-Id: I34fd83487d3ffe4eea58ddab397ef72acc866ac8

--- aosp_taimen.mk
+++ aosp_taimen.mk
@@ -14,6 +14,11 @@
 # limitations under the License.
 #
 
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+
 $(call inherit-product, device/google/taimen/device.mk)
 $(call inherit-product-if-exists, vendor/google_devices/taimen/device-vendor.mk)
 
