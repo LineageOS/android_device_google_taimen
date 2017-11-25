d49b4eaf3f65c4b67b348cde44fbfdeabb38207a
Christopher N. Hesse <raymanfx@gmail.com>
2017-11-25 19:13:26 +0100

    taimen: Remove prebuilt kernel modules copy rules
    Change-Id: Ifc1320c085ab8c7670921b638c5e33754d6cfb6b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,28 +27,6 @@ BOARD_AVB_ENABLE := true
 # sepolicy
 BOARD_SEPOLICY_DIRS += device/google/taimen/sepolicy
 
-ifeq (,$(filter-out taimen_gcc, $(TARGET_PRODUCT)))
-# if TARGET_PRODUCT == taimen_gcc
-BOARD_VENDOR_KERNEL_MODULES += \
-    device/google/wahoo-kernel/gcc/touch_core_base.ko \
-    device/google/wahoo-kernel/gcc/ftm4.ko \
-    device/google/wahoo-kernel/gcc/sw49408.ko \
-    device/google/wahoo-kernel/gcc/lge_battery.ko
-else ifeq (,$(filter-out taimen_kasan, $(TARGET_PRODUCT)))
-# if TARGET_PRODUCT == taimen_kasan
-BOARD_VENDOR_KERNEL_MODULES += \
-    device/google/wahoo-kernel/kasan/touch_core_base.ko \
-    device/google/wahoo-kernel/kasan/ftm4.ko \
-    device/google/wahoo-kernel/kasan/sw49408.ko \
-    device/google/wahoo-kernel/kasan/lge_battery.ko
-else
-BOARD_VENDOR_KERNEL_MODULES += \
-    device/google/wahoo-kernel/touch_core_base.ko \
-    device/google/wahoo-kernel/ftm4.ko \
-    device/google/wahoo-kernel/sw49408.ko \
-    device/google/wahoo-kernel/lge_battery.ko
-endif
-
 -include vendor/google_devices/taimen/proprietary/BoardConfigVendor.mk
 
 # Testing related defines
