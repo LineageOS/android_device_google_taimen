82424c8f5c04abfe7a28aa4182a7df52f22c3371
Christopher N. Hesse <raymanfx@gmail.com>
2017-11-25 13:39:47 +0100

    taimen: Initial Lineage makefile
    Change-Id: Ida34197a0d8e478941f2f80234ebb90b64b6d65b

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,3 +16,4 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_taimen.mk \
+    $(LOCAL_DIR)/lineage_taimen.mk \
---
+++ lineage_taimen.mk
@@ -0,0 +1,26 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2880
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/taimen/aosp_taimen.mk)
+
+-include device/google/wahoo/device-lineage.mk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_taimen
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 2 XL
+TARGET_MANUFACTURER := LG
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=taimen \
+    PRIVATE_BUILD_DESC="taimen-user 9 PPR1.180610.009 4898911 release-keys"
+
+BUILD_FINGERPRINT := google/taimen/taimen:9/PPR1.180610.009/4898911:user/release-keys
+
+$(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
