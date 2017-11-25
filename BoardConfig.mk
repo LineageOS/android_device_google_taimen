5ff19f0ccb54d843e314082f51bc40c1faea6145
Christopher N. Hesse <raymanfx@gmail.com>
2017-11-25 19:13:26 +0100

    taimen: Remove prebuilt kernel modules copy rules
    Change-Id: Ifc1320c085ab8c7670921b638c5e33754d6cfb6b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,56 +28,6 @@ BOARD_EXT4_SHARE_DUP_BLOCKS := true
 # sepolicy
 BOARD_SEPOLICY_DIRS += device/google/taimen/sepolicy
 
-# Kernel modules
-ifeq (,$(filter-out taimen_kasan, $(TARGET_PRODUCT)))
-# if TARGET_PRODUCT == taimen_kasan
-BOARD_VENDOR_KERNEL_MODULES += \
-    device/google/wahoo-kernel/kasan/touch_core_base.ko \
-    device/google/wahoo-kernel/kasan/ftm4.ko \
-    device/google/wahoo-kernel/kasan/sw49408.ko \
-    device/google/wahoo-kernel/kasan/lge_battery.ko \
-    device/google/wahoo-kernel/kasan/wlan.ko
-else ifeq (,$(filter-out taimen_kernel_debug_memory, $(TARGET_PRODUCT)))
-# if TARGET == taimen_kernel_debug_memory
-BOARD_VENDOR_KERNEL_MODULES += \
-    device/google/wahoo-kernel/debug_memory/touch_core_base.ko \
-    device/google/wahoo-kernel/debug_memory/ftm4.ko \
-    device/google/wahoo-kernel/debug_memory/sw49408.ko \
-    device/google/wahoo-kernel/debug_memory/lge_battery.ko \
-    device/google/wahoo-kernel/debug_memory/wlan.ko
-else ifeq (,$(filter-out taimen_kernel_debug_locking, $(TARGET_PRODUCT)))
-# if TARGET == taimen_kernel_debug_locking
-BOARD_VENDOR_KERNEL_MODULES += \
-    device/google/wahoo-kernel/debug_locking/touch_core_base.ko \
-    device/google/wahoo-kernel/debug_locking/ftm4.ko \
-    device/google/wahoo-kernel/debug_locking/sw49408.ko \
-    device/google/wahoo-kernel/debug_locking/lge_battery.ko \
-    device/google/wahoo-kernel/debug_locking/wlan.ko
-else ifeq (,$(filter-out taimen_kernel_debug_hang, $(TARGET_PRODUCT)))
-# if TARGET == taimen_kernel_debug_hang
-BOARD_VENDOR_KERNEL_MODULES += \
-    device/google/wahoo-kernel/debug_hang/touch_core_base.ko \
-    device/google/wahoo-kernel/debug_hang/ftm4.ko \
-    device/google/wahoo-kernel/debug_hang/sw49408.ko \
-    device/google/wahoo-kernel/debug_hang/lge_battery.ko \
-    device/google/wahoo-kernel/debug_hang/wlan.ko
-else ifeq (,$(filter-out taimen_kernel_debug_api, $(TARGET_PRODUCT)))
-# if TARGET == taimen_kernel_debug_api
-BOARD_VENDOR_KERNEL_MODULES += \
-    device/google/wahoo-kernel/debug_api/touch_core_base.ko \
-    device/google/wahoo-kernel/debug_api/ftm4.ko \
-    device/google/wahoo-kernel/debug_api/sw49408.ko \
-    device/google/wahoo-kernel/debug_api/lge_battery.ko \
-    device/google/wahoo-kernel/debug_api/wlan.ko
-else
-BOARD_VENDOR_KERNEL_MODULES += \
-    device/google/wahoo-kernel/touch_core_base.ko \
-    device/google/wahoo-kernel/ftm4.ko \
-    device/google/wahoo-kernel/sw49408.ko \
-    device/google/wahoo-kernel/lge_battery.ko \
-    device/google/wahoo-kernel/wlan.ko
-endif
-
 -include vendor/google_devices/taimen/proprietary/BoardConfigVendor.mk
 
 # Testing related defines
