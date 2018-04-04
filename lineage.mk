224ff841ede8e7b047fa40f2dcd8306c27d45252
Abhisek Devkota <ciwrl@lineageos.org>
2018-04-04 09:33:59 -0700

    Swap to new Fingerprint override mechanism
    Change-Id: Ie41d3b28dc8738059b740fc36249d9868c61ef61

--- lineage.mk
+++ lineage.mk
@@ -19,7 +19,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
-    BUILD_FINGERPRINT=google/taimen/taimen:8.1.0/OPM2.171019.029/4657601:user/release-keys \
     PRIVATE_BUILD_DESC="taimen-user 8.1.0 OPM2.171019.029 4657601 release-keys"
+
+BUILD_FINGERPRINT := google/taimen/taimen:8.1.0/OPM2.171019.029/4657601:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
