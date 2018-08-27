026c1ead2a5c328a2dd0bf5e7730d4cf077d6b66
Draymond1987 <38705844+Draymond1987@users.noreply.github.com>
2018-08-26 20:27:47 -0400

    Update generate-factory-images-package.sh

--- factory-images/generate-factory-images-package.sh
+++ factory-images/generate-factory-images-package.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-# Copyright 2017 The Android Open Source Project
+ # Copyright 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,8 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-source ../../../common/clear-factory-images-variables.sh
+ source ../../../common/clear-factory-images-variables.sh
 BUILD=4169385
 DEVICE=taimen
 PRODUCT=taimen
