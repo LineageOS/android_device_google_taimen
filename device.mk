e8ba1ac9750faf461628f22e11c74dd0cca225d9
Vincent Palomares <paillon@google.com>
2018-11-28 14:57:52 -0800

    Upgrading Taimen's thermal HAL version to 2.0.
    Also increasing thermal-engine skin shutdown threshold to let the framework
    shutdown gracefully (see b/119221890).
    
    Bug: 118689758
    Test: adb shell dumpsys hardware_properties
          adb shell su 0 lshal debug android.hardware.thermal@2.0::IThermal/default
          pts -m PtsThermalHalTestCases
          vts -m VtsHalThermalV1_0Target
          vts -m VtsHalThermalV2_0Target
    Change-Id: I3614e66df05844a048fb3f387c8fa0c9e0e67a68
    Signed-off-by: Vincent Palomares <paillon@google.com>

--- device.mk
+++ device.mk
@@ -59,6 +59,11 @@ PRODUCT_COPY_FILES += \
     device/google/taimen/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
     device/google/taimen/thermal-engine-vr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-vr.conf
 
+# Thermal HAL
+PRODUCT_COPY_FILES += \
+    device/google/taimen/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json \
+    device/google/taimen/thermal_info_config_evt.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_evt.json
+
 # Audio
 PRODUCT_COPY_FILES += \
     device/google/taimen/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil_taimen.xml \
