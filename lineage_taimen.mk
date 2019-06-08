031cd78ddd9e29dfd7a91004ad99acdbdbcf0185
Rashed Abdel-Tawab <rashed@linux.com>
2019-06-08 12:38:29 -0700

    taimen: Update to June fingerprint
    Change-Id: I2d5396770cb1fbff5c3e60683ec4648bef47e9d4

--- lineage_taimen.mk
+++ lineage_taimen.mk
@@ -19,8 +19,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
-    PRIVATE_BUILD_DESC="taimen-user 9 PPR1.180610.009 4898911 release-keys"
+    PRIVATE_BUILD_DESC="taimen-user 9 PQ3A.190605.003 5524043 release-keys"
 
-BUILD_FINGERPRINT := google/taimen/taimen:9/PPR1.180610.009/4898911:user/release-keys
+BUILD_FINGERPRINT := google/taimen/taimen:9/PQ3A.190605.003/5524043:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
