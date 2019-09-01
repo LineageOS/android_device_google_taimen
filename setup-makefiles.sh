83d1f4e674c651f8b60fb1ec40b33084010796cc
Christopher N. Hesse <raymanfx@gmail.com>
2019-09-01 14:28:52 +0200

    taimen: Add vendor blobs to list
    Change-Id: Ib9f5fd3999cca9e8a26645058e4f88d67d5db3c2
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -56,7 +56,7 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 write_taimen_headers
 
 # The standard blobs
-write_makefiles "$MY_DIR"/lineage-proprietary-files.txt
+write_makefiles "$MY_DIR"/lineage-proprietary-files.txt true
 
 # Done
 write_footers
