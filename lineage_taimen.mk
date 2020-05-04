d3aca89a47fce4c324e3cfc96ad6e7e2da083489
Eamon Powell <eamonpowell@outlook.com>
2020-05-05 09:22:05 +1000

    taimen: Update build fingerprint to QQ2A.200501.001.B3
    Change-Id: Ia01dc3a8838aaa9e2b2f6d5c836ecef39c026c3a

--- lineage_taimen.mk
+++ lineage_taimen.mk
@@ -18,8 +18,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
-    PRIVATE_BUILD_DESC="taimen-user 10 QQ2A.200405.005 6254899 release-keys"
+    PRIVATE_BUILD_DESC="taimen-user 10 QQ2A.200501.001.B3 6396602 release-keys"
 
-BUILD_FINGERPRINT := google/taimen/taimen:10/QQ2A.200405.005/6254899:user/release-keys
+BUILD_FINGERPRINT := google/taimen/taimen:10/QQ2A.200501.001.B3/6396602:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
