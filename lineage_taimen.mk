920da6cd1286306b46f0fef28838de405acd155e
Eamon Powell <eamonpowell@outlook.com>
2020-06-30 20:14:34 +1000

    taimen: Update build fingerprint to PQ3A.190801.002
    Change-Id: I484545145353d324d760c9c28eb5ef74efcd34f4

--- lineage_taimen.mk
+++ lineage_taimen.mk
@@ -18,8 +18,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
-    PRIVATE_BUILD_DESC="taimen-user 9 PQ3A.190605.003 5524043 release-keys"
+    PRIVATE_BUILD_DESC="taimen-user 9 PQ3A.190801.002 5670241 release-keys"
 
-BUILD_FINGERPRINT := google/taimen/taimen:9/PQ3A.190605.003/5524043:user/release-keys
+BUILD_FINGERPRINT := google/taimen/taimen:9/PQ3A.190801.002/5670241:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
