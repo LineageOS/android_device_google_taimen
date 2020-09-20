0f713fdd2917e92d3f72609e8afdbd9b376b7e65
Eamon Powell <eamonpowell@outlook.com>
2020-09-20 10:22:28 +1000

    taimen: Add tas2557_cal.bin symlink
    Change-Id: I4d253ba7e29c20ce1533dcfbf04470eae0d3a4d2

---
+++ Android.mk
@@ -0,0 +1,40 @@
+#
+# Copyright 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This contains the module build definitions for the hardware-specific
+# components for this device.
+#
+# As much as possible, those components should be built unconditionally,
+# with device-specific names to avoid collisions, to avoid device-specific
+# bitrot and build breakages. Building a component unconditionally does
+# *not* include it on all devices, so it is safe even with hardware-specific
+# components.
+
+ifneq ($(filter taimen, $(TARGET_DEVICE)),)
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+TAS2557_BIN := tas2557_cal.bin
+TAS2557_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(TAS2557_BIN)))
+$(TAS2557_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating tas2557_cal.bin symlink: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /persist/audio/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(TAS2557_SYMLINKS)
+
+endif
