055f73996d3cee02de068edbf312b9a021ddb52f
Michael Bestas <mkbestas@lineageos.org>
2025-03-17 16:17:04 +0200

    taimen: Temporarily disable ELF checks for some files
    Until display HAL is converted to blueprint
    
    Change-Id: I6fd49811118b4d4a81eea220363c77256ec6c08a

--- extract-files.py
+++ extract-files.py
@@ -16,6 +16,7 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
+    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -36,6 +37,12 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
+    (
+        'gralloc.msm8998',
+        'libqdMetaData',
+        'libqservice',
+        'libsdmutils',
+    ): lib_fixup_remove,
 }
 
 blob_fixups: blob_fixups_user_type = {
