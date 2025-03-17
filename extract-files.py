c2bf5260c5bc79b5d5be5664e61617b451b29d8a
Michael Bestas <mkbestas@lineageos.org>
2025-03-17 15:43:23 +0200

    DNM disable fw extract
    Change-Id: Ia0bd705f0b47dcda57e0248cd4dd7c6a9b008d14

--- extract-files.py
+++ extract-files.py
@@ -98,7 +98,7 @@ blob_fixups: blob_fixups_user_type = {
 
 extract_fns: extract_fns_user_type = {
     pixel_factory_image_regex: extract_pixel_factory_image,
-    pixel_firmware_regex: extract_pixel_firmware,
+    #pixel_firmware_regex: extract_pixel_firmware,
 }
 
 module = ExtractUtilsModule(
@@ -107,7 +107,7 @@ module = ExtractUtilsModule(
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
-    add_firmware_proprietary_file=True,
+    #add_firmware_proprietary_file=True,
     extract_fns=extract_fns,
 )
 
