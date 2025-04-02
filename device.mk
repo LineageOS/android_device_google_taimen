a81a77aed6341bf1375b74f472ce5d07875ca7f8
Nolen Johnson <johnsonnolen@gmail.com>
2025-04-02 01:15:17 -0400

    taimen: Drop ENG addons
    Change-Id: Ia328b0e5a6605b1899390f3b8e2054f7ddffc7e5

--- device.mk
+++ device.mk
@@ -125,16 +125,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
-# Enable modem logging
-ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.modem.diag.qdb=0\
-    persist.sys.modem.diag.mdlog=true \
-    persist.sys.modem.diag.mdlog_br_num=5 \
-    ro.radio.log_loc="/data/vendor/modem_dump" \
-    ro.radio.log_prefix="modem_log_"
-endif
-
 #IMU calibration
 PRODUCT_PROPERTY_OVERRIDES += \
   persist.config.calibration_fac=/persist/sensors/calibration/calibration.xml
